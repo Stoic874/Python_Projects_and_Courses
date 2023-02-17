402652ccb3268d38e3054e1a8fbdfe5b633c6b65
CS_Stoic <111648184+Stoic874@users.noreply.github.com>
2023-02-17 12:15:35 -0500

    Adding the files that will be needed. Contain no content yet.

---
+++ Google_Python_Course/final/changeImage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+
+from PIL import Image
+
+# Change image resolution from 3000x2000 to 600x400
+# Convert to RGB to change the format
+# Change image format from .TIFF to .JPEG
+# Save the images in ~/supplier-data/images
+
+
+
--- Google_Python_Course/final/final.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-from PIL import Image
-import sys
