e85bfb8bca16c3e6fad6d46305abfa171ee37ad6
CS_Stoic <111648184+Stoic874@users.noreply.github.com>
2023-02-17 13:33:57 -0500

    Get directory from intput argument, iterate throught the directory, resize and convert to jpeg.

--- Google_Python_Course/final/changeImage.py
+++ Google_Python_Course/final/changeImage.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3 
 
+import os
 from PIL import Image
+import sys
 
 # Change image resolution from 3000x2000 to 600x400
 # Convert to RGB to change the format
 # Change image format from .TIFF to .JPEG
 # Save the images in ~/supplier-data/images
 
+dir = sys.argv[1]
+files = os.listdir(dir)
+for f in files:
+    path = os.path.join(dir, f)
+    if path[0] == '.':
+        continue
+    else:
+        im = Image.open(path)
+        new_im = im.resize((600, 400)).convert('RGB').save("~/supplier-data/images/{}".format(f), format='jpeg')
 
 
